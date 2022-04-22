355a9dd4df79b640b66d457c33c3569986bf1d4f
TommasoBarberis <tommaso.barberis@etu.univ-lyon1.fr>
2022-04-22 15:43:10 +0200

    update

--- scripts/library_construction/utils/test_extender.sh
+++ scripts/library_construction/utils/test_extender.sh
@@ -19,37 +19,37 @@ export fasta_file=$(realpath $1)
 work_dir=$(realpath .)
 genome_file=$(realpath $2)
 
-# python3 - << EOF
-# from Bio import SeqIO
-# import os
+python3 - << EOF
+from Bio import SeqIO
+import os
 
-# c = 0
-# for seq_record in SeqIO.parse("$fasta_file", "fasta"):
-#     os.mkdir("seq_" + str(c))
+c = 0
+for seq_record in SeqIO.parse("$fasta_file", "fasta"):
+    os.mkdir("seq_" + str(c))
 
-#     with open("seq_" + str(c) + "/seq.fasta", "w") as f:
-#         f.write(">sequence\n" + str(seq_record.seq))
-#     c += 1
-# EOF
+    with open("seq_" + str(c) + "/seq.fasta", "w") as f:
+        f.write(">sequence\n" + str(seq_record.seq))
+    c += 1
+EOF
 
-# for dir in $(ls -d -1 $work_dir/seq_*); do
+for dir in $(ls -d -1 $work_dir/seq_*); do
 
-#     echo -e "\npreparatory stuff \n $dir \n\n"
-#     cd $(realpath $dir)
-#     make_fasta_from_blast.sh $genome_file seq.fasta 80 1500
-#     nb_seq=$(grep -c ">" seq.fasta.blast.bed.fa)
-#     if [ $nb_seq -gt 200 ]; then
-#         ready_for_MSA.sh seq.fasta.blast.bed.fa 200 50
-#         mafft --reorder --thread $3 seq.fasta.blast.bed.fa.rdmSubset.fa > maf.fa
-#     else
-#         mafft --reorder --thread $3 seq.fasta.blast.bed.fa > maf.fa
-#     fi
+    echo -e "\npreparatory stuff \n $dir \n\n"
+    cd $(realpath $dir)
+    make_fasta_from_blast.sh $genome_file seq.fasta 80 1500
+    nb_seq=$(grep -c ">" seq.fasta.blast.bed.fa)
+    if [ $nb_seq -gt 200 ]; then
+        ready_for_MSA.sh seq.fasta.blast.bed.fa 200 50
+        mafft --reorder --thread $3 seq.fasta.blast.bed.fa.rdmSubset.fa > maf.fa
+    else
+        mafft --reorder --thread $3 seq.fasta.blast.bed.fa > maf.fa
+    fi
     
-#     echo -e "\n CIAlign stuff \n\n"
-#     CIAlign --infile maf.fa --outfile_stem no_clean --make_consensus --plot_output --plot_format svg
-#     CIAlign --infile maf.fa --outfile_stem no_ins --remove_insertions --make_consensus --plot_output --plot_format svg
+    echo -e "\n CIAlign stuff \n\n"
+    CIAlign --infile maf.fa --outfile_stem no_clean --make_consensus --plot_output --plot_format svg
+    CIAlign --infile maf.fa --outfile_stem no_ins --remove_insertions --make_consensus --plot_output --plot_format svg
 
-# done
+done
 
 conda activate te_aid
 
